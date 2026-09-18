9d72a7f530d34004a2c3acf2ea4199177b42d4ef
Davi de Jesus <davi.santosoliveira214@gmail.com>
2026-09-18 19:49:54 -0300

    feat(skills): adiciona detalhes das tecnologias e carrossel infinito

--- src/components/Sessoes/SkillsSessao.jsx
+++ src/components/Sessoes/SkillsSessao.jsx
@@ -1,30 +1,86 @@
+import { useEffect, useRef } from 'react'
+
 import Linguagem from '../cards/CardLinguagem'
 import { linguagens } from '../../data/techs'
 import TituloSessao from '../Titulos'
 
 export default function SkillsSessao({ titulo = 'Conhecimentos' }) {
+   const divRef = useRef(null)
+
+   useEffect(() => {
+      const limparEvento = eventoCarrossel(divRef)
+
+      return () => {
+         if (limparEvento) limparEvento()
+      }
+   }, [])
+
    return (
       <div className='flex flex-col gap-5 px-4'>
          <TituloSessao texto={titulo} />
-         <div className='w-full overflow-hidden mask-l-from-80% mask-r-from-80%'>
-            <div className='flex gap-3.5 w-max px-5 animate-scrollInfinito'>
+         <div className='w-full overflow-x-hidden mask-l-from-80% mask-r-from-80%'>
+            <div
+               className={`flex gap-3.5 w-max px-5 animate-scrollInfinito`}
+               ref={divRef}
+            >
                {linguagens.map((iten) => (
-                  <Linguagem
-                     key={`origin-${iten.id}`}
-                     icone={iten.img}
-                     linguagem={iten.nome}
-                  />
+                  <Linguagem key={`origin-${iten.id}`} info={iten} />
                ))}
 
                {linguagens.map((iten) => (
-                  <Linguagem
-                     key={`clone-${iten.id}`}
-                     icone={iten.img}
-                     linguagem={iten.nome}
-                  />
+                  <Linguagem key={`clone-${iten.id}`} info={iten} />
                ))}
             </div>
          </div>
       </div>
    )
+}
+
+function eventoCarrossel(ref) {
+   const elemento = ref.current
+   if (!elemento) return
+
+   const pausarAnimacao = () => {
+      elemento.style.animationPlayState = 'paused'
+   }
+
+   const rodarAnimacao = () => {
+      elemento.style.animationPlayState = 'running'
+   }
+
+   const rodarPausarMobile = (e) => {
+      if (window.innerWidth >= 768) return
+
+      const estaPausado = elemento.style.animationPlayState === 'paused'
+      e.preventDefault()
+
+      !estaPausado ? pausarAnimacao() : rodarAnimacao()
+   }
+
+   const handleMouseEnter = () => {
+      if (window.innerWidth >= 768) pausarAnimacao()
+   }
+
+   const handleMouseLeave = () => {
+      if (window.innerWidth >= 768) rodarAnimacao()
+   }
+
+   const filhas = Array.from(elemento.children)
+   filhas.forEach((filha) => {
+      filha.addEventListener('mouseenter', handleMouseEnter)
+
+      filha.addEventListener('mouseleave', handleMouseLeave)
+
+      filha.addEventListener('touchstart', rodarPausarMobile, {
+         passive: false,
+      })
+   })
+
+   return () => {
+      filhas.forEach((filha) => {
+         filha.removeEventListener('mouseenter', handleMouseEnter)
+         filha.removeEventListener('mouseleave', handleMouseLeave)
+         filha.removeEventListener('touchstart', rodarPausarMobile)
+      })
+   }
 }
--- src/components/cards/CardLinguagem.jsx
+++ src/components/cards/CardLinguagem.jsx
@@ -1,10 +1,36 @@
-export default function CardLinguagem({ icone, linguagem }) {
+import { useState } from 'react'
+
+export default function CardLinguagem({ info }) {
+   const { nome, img, sobre, cor } = info
+   const [aberto, setAberto] = useState(false)
+
    return (
-      <div className='flex flex-col min-w-48 bg-charcoal-blue-900 py-2 gap-2.5 rounded-2xl'>
-         <img src={icone} alt='' className='mx-auto w-24' />
-         <p className='text-2xl text-extra-branco text-center font-bold'>
+      <div
+         className='relative flex group w-48'
+         onMouseEnter={() => setAberto(true)}
+         onMouseLeave={() => setAberto(false)}
+         onTouchStart={() => setAberto(!aberto)}
+      >
+         <div className='flex flex-col w-full bg-charcoal-blue-900 py-2 gap-2.5 rounded-2xl'>
+            <img src={img} alt='' className='mx-auto w-24' />
+            <p className='text-2xl text-extra-branco text-center font-bold'>
+               {nome}
+            </p>
+         </div>
+         <Info linguagem={nome} sobre={sobre} cor={cor} aberto={aberto} />
+      </div>
+   )
+}
+
+function Info({ linguagem, sobre, cor, aberto }) {
+   return (
+      <div
+         className={`absolute ${aberto ? 'flex' : 'hidden'} flex-col justify-center inset-0 bg-charcoal-blue-900 px-1.5 gap-2.5 rounded-2xl`}
+      >
+         <p className='text-center text-xl font-bold' style={{ color: cor }}>
             {linguagem}
          </p>
+         <p className='text-center text-extra-branco text-sm'>{sobre}</p>
       </div>
    )
 }
